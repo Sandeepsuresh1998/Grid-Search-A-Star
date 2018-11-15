6842bf2ce2aee4ff2a9022fde385a4a23e8e5e15
Sandeep Suresh <sureshsa@usc.edu>
2018-11-14 17:30:54 -0800

    Fleshed out the code for the A* alg. The adaptive part is still unfinished.

--- GridPathPlanner.cpp
+++ GridPathPlanner.cpp
@@ -1,6 +1,8 @@
 #include "GridPathPlanner.h"
 #include <set>
 #include <queue>
+#include <map>
+#include <algorithm>
 
 using namespace std;
 
@@ -29,7 +31,11 @@ void GridPathPlanner::FindPath(xyLoc start, std::vector<xyLoc> & path) {
 	//Creating the open set with pairs of f-value and Locations
 	//This allows for sorting by smallest f-value
 	priority_queue< pair< int, xyLoc > , vector< pair< int, xyLoc> >, greater< pair< int, xyLoc > > > open_set;
-	open_set.push(make_pair(GetHValue(start), start));
+	open_set.push(make_pair(GetHValue(start), start)); //No g(s) bc it is the start node
+
+	//Map to remember where each cell came from key = xyloc, value = where xyloc came from
+	map < xyLoc, xyLoc > path_map;
+	path_map.insert(make_pair(start, start));
 
 	// //TODO: We need to be able to trace back the path, so having a map where each thing location came from would be useful
 
@@ -37,8 +43,75 @@ void GridPathPlanner::FindPath(xyLoc start, std::vector<xyLoc> & path) {
 	//Keep iterating until we have no more nodes to explore. 
 	//If we find the goal state we break.
 	while(!open_set.empty()) {
-		xyLoc current = (open_set.top()).second; //Let's explore the first element in queue 
-		open_set.pop() // Clear it from open set
+		//Let's explore the first element in queue and clear it from the open set
+		xyLoc current = (open_set.top()).second; 
+		open_set.pop();
+
+		//Now that we have seen this node at it to the closed set
+		closed_set.insert(current);
+
+		//We found goal state
+		if(current == destination) {
+
+			path.push_back(current);
+
+			//Creating the path
+			while(path_map[current] != start) {
+				//Add where the cell came from
+				path.push_back(path_map[current]);
+
+				//Update current
+				current = path_map[current];
+			}
+
+			//Add the starting cell
+			path.push_back(start);
+
+			//We need to reverse this
+			reverse(path.begin(), path.end());
+
+			return;
+
+		}
+
+		//Let's explore all neighbors of the current loc. 
+		vector<xyLoc> neighbors; 
+		//Left
+		neighbors.push_back(xyLoc(current.x - 1, current.y));
+		
+		//Up
+		neighbors.push_back(xyLoc(current.x, current.y + 1));
+		
+		//Right
+		neighbors.push_back(xyLoc(current.x + 1, current.y));
+	
+		//Down
+		neighbors.push_back(xyLoc(current.x, current.y + 1));
+		
+
+		//Iterate through the neighbors and add them to the lists
+		for(int i = 0; i < neighbors.size(); i++) {
+			// If it's not a valid location skip it
+			if(!(grid->IsValidLocation(neighbors[i]))) {
+				continue;
+			} else {
+				//If location is blocked
+				if(!(grid->IsBlocked(neighbors[i]))) {
+					//If we spot hasn't been searched before, let's add it to the list
+					if(closed_set.find(neighbors[i]) == closed_set.end()) {
+						open_set.push(make_pair(GetHValue(neighbors[i]) , neighbors[i]));
+						closed_set.insert(neighbors[i]);
+					} else {
+						continue;
+					}
+				} else {
+					//Still add this to the closed set so we don't explore it again
+					closed_set.insert(neighbors[i]);
+				}
+			}
+
+		}
+
 	}
 
 
